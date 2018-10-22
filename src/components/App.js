011178c24d384b49a18adb260bff65d49994348b
Rachel Inkala <rachelinkala@gmail.com>
2018-10-22 12:26:39 -0600

    add react-redux

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Ledger from './Ledger'
+import WishList from './Wishlist'
+
+const App = () => (
+  <div>
+    <Ledger />
+    <WishList />
+  </div>
+)
+
+export default () => <div></div>
---
+++ src/components/Wishlist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WishListForm from './WishListForm'
+import Items from './Items'
+
+const WishList = () => (
+  <div>
+    <WishListForm />
+    <Items />
+  </div>
+)
+
+export default () => <div></div>
